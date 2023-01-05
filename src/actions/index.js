410a8c20082880daf1a9dcb8cf33a0eaf2cc6bde
panospro <panagiotisskaki@yahoo.gr>
2023-01-05 13:38:22 +0200

    Documentation for actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+/*
+* Import auth and ui, give the values into actions and export it
+*/
 import auth from './auth';
 import ui from './ui';
 
--- src/actions/ui.js
+++ src/actions/ui.js
@@ -1,8 +1,13 @@
+/*
+* Initialize ui and export it
+*/
 const ui = {
+    // Create an action object with type set to 'UI.ADD_ERROR' and payload set to error
     addError: (error) => ({
         type: 'UI.ADD_ERROR',
         payload: error
     }),
+    // Create an action object with type set to 'UI.REMOVE_ERROR' and payload set to id
     removeError: (id) => ({
         type: 'UI.REMOVE_ERROR',
         payload: id
