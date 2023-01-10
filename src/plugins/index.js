247a84fa8b0c01d3a348c04f6e78f9e06275ec0f
panospro <panagiotisskaki@yahoo.gr>
2023-01-10 21:21:18 +0200

    Plugins doc

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,3 +1,6 @@
+/*
+* Import ramda and initialize-store and export a compose version of initializeStore.
+*/
 import {compose} from 'ramda';
 import initializeStore from './initialize-store';
 
--- src/plugins/initialize-store.js
+++ src/plugins/initialize-store.js
@@ -1,9 +1,15 @@
+/*
+* Import the necessery modules
+*/
 import {compose, createStore} from 'redux';
 import {persistStore} from 'redux-persist';
 import rootReducer from '../reducers';
 
+// Checks to see if the window object has an extension on it that allows for the use.
+// If it does, then it uses the Redux DevTools extension, otherwise it uses the compose function.
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// Creates a store and a persistor and returns them as an object.
 const create = (reducer, initialState) => {
     const store = createStore(reducer, initialState, composeEnhancers());
     const persistor = persistStore(store);
@@ -11,6 +17,7 @@ const create = (reducer, initialState) => {
     return {store, persistor};
 };
 
+// Sets the initial state of auth
 const {store, persistor} = create(rootReducer, {
     auth: {
         user: {},
@@ -18,7 +25,9 @@ const {store, persistor} = create(rootReducer, {
     }
 });
 
+// Initializes a store object with the given configuration and then passes it to the next function.
 const initializeStore = (next) => (configuration) => next({...configuration, store, persistor});
 
+// Export store and persistor as one object and default export initializeStore
 export {store, persistor};
 export default initializeStore;
