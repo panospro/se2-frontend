34610aefa655f2345bbe3d2b78998ffbae4a1af0
panospro <panagiotisskaki@yahoo.gr>
2023-02-20 22:44:21 +0200

    12

--- src/components/dashboard/components/value.js
+++ src/components/dashboard/components/value.js
@@ -133,6 +133,7 @@ class Value extends React.Component {
         } catch {}
     }
 
+
     
     // Connects to a Stomp source. It uses a login, passcode
     // and host from the source to create a stompConfig.
