e36cc7c3c8f67e6b5b1e2c2c59b33be0cde7283d
panospro <panagiotisskaki@yahoo.gr>
2023-02-22 14:42:36 +0200

    Update sources.js

--- src/api/sources.js
+++ src/api/sources.js
@@ -4,46 +4,52 @@ import { api, prefixUrl } from '../lib/api-adapter';
 // Initialize the sources API with the appropriate prefix URL.
 const sourcesApi = api.extend({ prefixUrl: prefixUrl('sources') });
 
-/*
+/**
  * Retrieves a list of sources.
- * Returns a promise that resolves with the list of sources.
+ *
+ * @returns {Promise} A Promise that resolves to the list of sources.
  */
 export const getSources = () => sourcesApi.get('sources').json();
 
-/*
+/**
  * Creates a new source.
- * data - The data for the new source.
- * Returns a promise that resolves with the new source.
+ *
+ * @param {Object} data - The data for the new source.
+ * @returns {Promise} A Promise that resolves to the newly created source.
  */
 export const createSource = (data) => sourcesApi.post('create-source', {json: data}).json();
 
-/*
+/**
  * Changes an existing source.
- * data - The data for the source.
- * oldId - The old ID of the source.
- * Returns a promise that resolves with the changed source.
+ *
+ * @param {Object} data - The new data for the source.
+ * @param {string} oldId - The ID of the source to change.
+ * @returns {Promise} A Promise that resolves to the updated source.
  */
 export const changeSource = (data, oldId) => sourcesApi.post('change-source', {json: {...data, id: oldId}}).json();
 
-/*
+/**
  * Deletes a source.
- * id - The ID of the source to delete.
- * Returns a promise that resolves with the deleted source.
+ *
+ * @param {string} id - The ID of the source to delete.
+ * @returns {Promise} A Promise that resolves to the deleted source.
  */
 export const deleteSource = (id) => sourcesApi.post('delete-source', {json: {id}}).json();
 
-/*
+/**
  * Finds a specific source.
- * name - The name of the source to find.
- * owner - The owner of the source to find.
- * user - The user of the source to find.
- * Returns a promise that resolves with the specified source.
+ *
+ * @param {string} name - The name of the source to find.
+ * @param {string} owner - The owner of the source to find.
+ * @param {string} user - The user of the source to find.
+ * @returns {Promise} A Promise that resolves to the found source.
  */
 export const findSource = (name, owner, user) => sourcesApi.post('source', {json: {name, owner, user}}).json();
 
-/*
+/**
  * Checks the status of a list of sources.
+ *
  * @param {Array} sources - The list of sources to check.
- * Returns a promise that resolves with the status of the sources.
+ * @returns {Promise} A Promise that resolves to the status of the sources.
  */
 export const checkSource = (sources) => sourcesApi.post('check-sources', {json: {sources}}).json();
